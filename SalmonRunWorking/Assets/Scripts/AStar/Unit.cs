a0ffd6587ee45d5bb6d588d527d0ed0983ab6147
GeneRainier <GeneRainier@gmail.com>
2020-12-24 16:11:11 -0500

    Added basic Intermediate Nodes
    Added basic functionality to have intermediate nodes randomly selected for each unit in the test scene. This does not seem to hurt performance and (with some fixes to navigating near obstacles) can be used to randomly have fish move to seemingly "non-optimal" locations in the river upon instantiation

--- SalmonRunWorking/Assets/Scripts/AStar/Unit.cs
+++ SalmonRunWorking/Assets/Scripts/AStar/Unit.cs
@@ -11,16 +11,35 @@ public class Unit : MonoBehaviour
     const float minPathUpdateTime = 0.2f;       ///< Rather than every frame, we will allow a path update no sooner than .2 seconds
     const float pathUpdateThreshold = 0.5f;     ///< Rather than every couple seconds, we will only allow a path update if the unit has moved a certain distance
     
-    public Transform target;        ///< Position of the target location
+    public Transform target;        ///< Position of the final, target location
+    public Transform[] targets;     ///< An array of the possible end points a unit may choose to end at
+    public Transform intermediateNode;          ///< Position if the required mid-point in the unit's path
+    public Transform[] intermediateNodes;       ///< An array of all the positions this unit may choose to visit before heading for the target
+    [SerializeField] private bool intermediateReached = false;   ///< Has this unit reached the intermediate node yet?
     public float speed = 20f;       ///< The speed of the unit
     public float turnSpeed = 3f;    ///< The speed at which the unit turns
     public float turnDistance = 5f; ///< The distance over which this unit will turn towards the next node
 
-    Path path;
+    [SerializeField] private AStarGrid theGrid;     ///< A reference to the grid so we can compare node positions for the player, intermediate node, and target
+    Path path;      ///< The path script this unit is following to smooth its movement
 
     private void Start()
     {
+        // Choose the intermediate node and target node to head towards prior to the final target and begin following the path
+        int intermediateIndex = Random.Range(0, intermediateNodes.Length);
+        int targetNodeIndex = Random.Range(0, targets.Length);
+        intermediateNode = intermediateNodes[intermediateIndex];
+        target = targets[targetNodeIndex];
         StartCoroutine(UpdatePath());
+    }
+
+    private void Update()
+    {
+        // Have we reached the intermediate node?
+        if (Vector3.Distance(transform.position, intermediateNode.position) <= 3)
+        {
+            intermediateReached = true;
+        }
     }
 
     /*
@@ -48,20 +67,59 @@ public class Unit : MonoBehaviour
         {
             yield return new WaitForSeconds(0.3f);
         }
-        PathRequestManager.RequestPath(new PathRequest(transform.position, target.position, OnPathFound));
+        
+        // If we have not yet visited the intermediate node, then we should continue pathing to there.
+        // Otherwise, we start pathing towards the final node
+        PathRequestManager.RequestPath(new PathRequest(transform.position, intermediateNode.position, OnPathFound));
 
         float squareMoveThreshold = pathUpdateThreshold * pathUpdateThreshold;
-        Vector3 targetPosOld = target.position;
-        
+        Vector3 targetPosOld = intermediateNode.position;
+
         while (true)
         {
             yield return new WaitForSeconds(minPathUpdateTime);
-            if ((target.position - targetPosOld).sqrMagnitude > squareMoveThreshold)
+            if (((intermediateNode.position - targetPosOld).sqrMagnitude > squareMoveThreshold) && intermediateReached == false)
+            {
+                PathRequestManager.RequestPath(new PathRequest(transform.position, intermediateNode.position, OnPathFound));
+                targetPosOld = intermediateNode.position;
+            }
+            else if (((target.position - targetPosOld).sqrMagnitude > squareMoveThreshold) && intermediateReached == true)
             {
                 PathRequestManager.RequestPath(new PathRequest(transform.position, target.position, OnPathFound));
                 targetPosOld = target.position;
             }
         }
+        //else
+        //{
+        //    PathRequestManager.RequestPath(new PathRequest(transform.position, target.position, OnPathFound));
+
+        //    float squareMoveThreshold = pathUpdateThreshold * pathUpdateThreshold;
+        //    Vector3 targetPosOld = target.position;
+
+        //    while (true)
+        //    {
+        //        yield return new WaitForSeconds(minPathUpdateTime);
+        //        if ((target.position - targetPosOld).sqrMagnitude > squareMoveThreshold)
+        //        {
+        //            PathRequestManager.RequestPath(new PathRequest(transform.position, target.position, OnPathFound));
+        //            targetPosOld = target.position;
+        //        }
+        //    }
+        //}
+        //PathRequestManager.RequestPath(new PathRequest(transform.position, target.position, OnPathFound));
+
+        //float squareMoveThreshold = pathUpdateThreshold * pathUpdateThreshold;
+        //Vector3 targetPosOld = target.position;
+        
+        //while (true)
+        //{
+        //    yield return new WaitForSeconds(minPathUpdateTime);
+        //    if ((target.position - targetPosOld).sqrMagnitude > squareMoveThreshold)
+        //    {
+        //        PathRequestManager.RequestPath(new PathRequest(transform.position, target.position, OnPathFound));
+        //        targetPosOld = target.position;
+        //    }
+        //}
     }
 
     /*
