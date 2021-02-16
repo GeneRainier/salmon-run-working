1ed07be9277aa1dba51d325bafd47c475cf5495c
17sjohns <61259988+17sjohns@users.noreply.github.com>
2021-02-15 21:21:57 -0500

    Adding the second map
    Added the shortened map to the screen, added some pop up things to the upgrade system.

---
+++ SalmonRunWorking/Assets/Scripts/UI/MenuSliderButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSliderButton : MonoBehaviour
+{
+    public GameObject LadderSlider;
+
+    public void OnButtonPress()
+    {
+        Debug.Log("buttonPressed");
+
+        if(LadderSlider != null)
+        {
+            Animator animator = LadderSlider.GetComponent<Animator>();
+
+            if (animator != null)
+            {
+                bool isOpen = animator.GetBool("SlideOut");
+                animator.SetBool("SlideOut", !isOpen);
+            }
+        }
+
+    }
+}
