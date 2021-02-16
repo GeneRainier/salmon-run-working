a4c683abe34bfd686e269947103140d70ef091a4
17sjohns <61259988+17sjohns@users.noreply.github.com>
2021-02-16 18:32:49 -0500

    worked on Upgrade Pannel
    Fixed up the upgrade pannel more.

---
+++ SalmonRunWorking/Assets/Scripts/Upgrading/PanelSwitching.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelSwitching : MonoBehaviour
+{
+    public RectTransform theRectTransform;
+
+    public void OnButtonPress ()
+    {
+        Debug.Log("buttonPressed");
+
+        //theRectTransform = transform as RectTransform; // Cast it to RectTransform
+        theRectTransform.SetAsLastSibling(); // Make the panel show on top.
+    }
+}
