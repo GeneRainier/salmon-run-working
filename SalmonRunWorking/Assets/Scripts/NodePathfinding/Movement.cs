ea11db582fba3defa3dd3c0d7ac067f2673cac89
GeneRainier <GeneRainier@gmail.com>
2021-05-25 19:11:24 -0400

    Finalized 3 methods
    Made three methods for the Fish Pathfinding. One generates a path from start to finish once a fish spawns in. The second has the path change its target location as it moves based on Raycasting and the third does the same thing based on Triggers

--- SalmonRunWorking/Assets/Scripts/NodePathfinding/Movement.cs
+++ SalmonRunWorking/Assets/Scripts/NodePathfinding/Movement.cs
@@ -10,57 +10,127 @@ public class Movement : MonoBehaviour
     [SerializeField] private float moveSpeed;   //< The speed this object is moving
     [SerializeField] private float turnSpeed;   //< The speed this object turns while moving
 
+    [SerializeField] private List<Destination> path;    //< A list of destinations that the fish will follow
+    private bool craftingPath = true;                   //< Whether or not this fish is still making a path to the end
+    private int currentIndex = 0;                        //< The index of the current node this fish is moving to
+
+    private int nodeLayerMask = 1 << 12;     //< The layer that the nodes are located on
+
+    private void Start()
+    {
+        spawner = FindObjectOfType<GenericSpawner>();
+
+        destination = spawner.initialDestinations[Random.Range(0, 5)];
+        Vector3 lookPosition = destination.destinationPosition - transform.position;
+        destinationDirection = Quaternion.LookRotation(lookPosition);
+        path.Add(destination);
+        Destination currentNode = destination;
+
+        while (craftingPath)
+        {
+            Destination newNode = currentNode.destinations[Random.Range(0, 5)];
+            if (newNode.finalDestination == true)
+            {
+                path.Add(newNode);
+                craftingPath = false;
+            }
+            else 
+            {
+                path.Add(newNode);
+                currentNode = newNode;
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (destination != null)
         {
+            if (transform.position == destination.destinationPosition)
+            {
+                if (destination.finalDestination == true)
+                {
+                    Destroy(this.gameObject);
+                }
+                else
+                {
+                    currentIndex++;
+                    destination = path[currentIndex];
+
+                    Vector3 lookPosition = destination.destinationPosition - transform.position;
+                    lookPosition.y = 0.0f;
+                    destinationDirection = Quaternion.LookRotation(lookPosition);
+                }
+            }
+
             // Move towards the destination at a constant speed
             transform.position = Vector3.MoveTowards(transform.position, destination.destinationPosition, moveSpeed * Time.deltaTime);
-            // Turn the fish to face the target at a constant speed
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, destinationDirection, turnSpeed * Time.deltaTime);
-        }
-        else if (spawner != null)
-        {
-            destination = spawner.initialDestinations[Random.Range(0, 5)];
-            Vector3 lookPosition = destination.destinationPosition - transform.position;
-            destinationDirection = Quaternion.LookRotation(lookPosition);
+
             float deltaAngle = Quaternion.Angle(transform.rotation, destinationDirection);
 
+            // Exit early if no update required
             if (deltaAngle == 0.00F)
-            { // Exit early if no update required
+            { 
                 return;
             }
 
+            // Turn the fish to face the target at a constant speed
             transform.rotation = Quaternion.Slerp(transform.rotation, destinationDirection, turnSpeed * Time.deltaTime / deltaAngle);
         }
-        else
-        {
-            spawner = FindObjectOfType<GenericSpawner>();
-        }
     }
+
+    // Only necessary for the Raycast Method
+    //private void FixedUpdate()
+    //{
+    //    // A recurring raycast check for this destination
+    //    RaycastHit hit;
+
+    //    // Does this raycast hit any of the fish?
+    //    if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, nodeLayerMask))
+    //    {
+    //        // Change the fishes destination
+    //        //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down), Color.yellow);
+    //        if (hit.collider.gameObject.GetComponent<Destination>().finalDestination == true)
+    //        {
+    //            Destroy(this.gameObject);
+    //        }
+    //        else
+    //        {
+    //            destination = destination.destinations[Random.Range(0, 5)];
+    //            Vector3 lookPosition = destination.destinationPosition - transform.position;
+    //            lookPosition.y = 0.0f;
+
+    //            destinationDirection = Quaternion.LookRotation(lookPosition);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down), Color.yellow);
+    //    }
+    //}
 
     // When we hit a trigger, check that it is our destination. If not, keep going to the destination
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.GetComponent<Destination>() == destination)
-        {
-            if (destination.finalDestination == true)
-            {
-                Destroy(this.gameObject);
-            }
-            else
-            {
-                destination = destination.destinations[Random.Range(0, 5)];
-                Vector3 lookPosition = destination.destinationPosition - transform.position;
-                lookPosition.y = 0.0f;
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.GetComponent<Destination>() == destination)
+    //    {
+    //        if (destination.finalDestination == true)
+    //        {
+    //            Destroy(this.gameObject);
+    //        }
+    //        else
+    //        {
+    //            destination = destination.destinations[Random.Range(0, 5)];
+    //            Vector3 lookPosition = destination.destinationPosition - transform.position;
+    //            lookPosition.y = 0.0f;
 
-                destinationDirection = Quaternion.LookRotation(lookPosition);
-            }
-        }
-        else
-        {
-            return;
-        }
-    }
+    //            destinationDirection = Quaternion.LookRotation(lookPosition);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        return;
+    //    }
+    //}
 }
