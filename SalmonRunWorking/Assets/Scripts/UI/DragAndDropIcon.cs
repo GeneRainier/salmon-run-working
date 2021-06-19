5de269541039224f21de3af52fda439edcd825b9
GeneRainier <GeneRainier@gmail.com>
2021-06-18 22:49:23 -0400

    Updated Tower Placement
    Adjusted tower placement parameters to allow for human towers to be placed closer to the shoreline. Also, updated some of the comments to be standardized across a few scripts.

--- SalmonRunWorking/Assets/Scripts/UI/DragAndDropIcon.cs
+++ SalmonRunWorking/Assets/Scripts/UI/DragAndDropIcon.cs
@@ -6,65 +6,61 @@ using UnityEngine.UI;
 /**
  * Script that controls a drag and drop icon for dragging towers onto the play area.
  * 
- * Author: Jacob Cousineau
+ * Original Author: Jacob Cousineau
+ * Subsequent Authors: Benjamin Person
  */
 [RequireComponent(typeof(RectTransform))]
 public class DragAndDropIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
-    // the object we should attempt to spawn
-    public GameObject objectToSpawn;  // if public it will show up in the inspector; this includes varibles that might be have a start or default value
+    public GameObject objectToSpawn;  //< The object we should attempt to spawn
+                                      // If public it will show up in the inspector; this includes varibles that might be have a start or default value
                                       // public color mycolor with show up as a color picker to get a start color
                                       // can put a hide inspector. [SerializeField] will also show up in inspector, but not be public
                                       // Serialize Field will also keep its value between runs. 
 
-    public Image icon;
+    public Image icon;                //< The image for this type of tower that shows in the UI
 
-    // the rect transform for the icon
-    private RectTransform rectTransform;
+    private RectTransform rectTransform;    //< The rect transform for the icon
 
-    // the instance of the object we will (attempt to) spawn
-    private GameObject spawnedObject;
+    private GameObject spawnedObject;       //< The instance of the object we will (attempt to) spawn
 
-    // layer the spawned object started on and will be restored to once placed
-    private LayerMask spawnedObjectLayer;
+    private LayerMask spawnedObjectLayer;   //< Layer the spawned object started on and will be restored to once placed
 
-    // the Tower interface attached to the spawned object
-    private IDragAndDropObject spawnedObjectDragAndDrop;
+    private IDragAndDropObject spawnedObjectDragAndDrop;    //< The Tower interface attached to the spawned object
 
-    // hold offsets for corners of spawned object renderer's bounding box
-    // used for raycasting from all four corners of the spawned object in addition to the center
+    // Hold offsets for corners of spawned object renderer's bounding box
+    // Used for raycasting from all four corners of the spawned object in addition to the center
     private Vector3 spawnedObjectOffsetTopLeft;
     private Vector3 spawnedObjectOffsetTopRight;
     private Vector3 spawnedObjectOffsetBottomLeft;
     private Vector3 spawnedObjectOffsetBottomRight;
 
-    // holds layer mask for raycasting
-    private int raycastLayerMask;
-    
+    private int raycastLayerMask;       //< Holds layer mask for raycasting
+
+    // Main camera and necessary manager script references
     private Camera mainCamera;
     private GameManager gameManager;
     private MoneyManager moneyManager;
     
-    //find the icon image to set its color
-    [SerializeField] private Image iconImage;
+    [SerializeField] private Image iconImage;   //< We find the icon image to set its color
 
-    private TowerUI towerUI;
+    private TowerUI towerUI;            //< Reference to the TowerUI script for necessary functions
 
+    // Boolean determinants for purchasing single purchase items like dams and ladders
     public bool isOneTimePurcahse;
     private bool hasBeenPurchased;
-
 
     /**
      * Initialization
      */
     void Start ()
     {
-        // get rect transform component
+        // Get rect transform component
         rectTransform = GetComponent<RectTransform>();
 
         towerUI = GetComponent<TowerUI>();
 
-        // get layer mask for necessary layers
+        // Get layer mask for necessary layers
         raycastLayerMask = LayerMask.GetMask(Layers.FLOOR_LAYER_NAME, Layers.TERRAIN_LAYER_NAME, Layers.PLACED_OBJECTS);
 
         mainCamera = Camera.main;
@@ -75,10 +71,13 @@ public class DragAndDropIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     private void Update()
     {
+        // Check each frame update for whether the player can afford the specific item
         CanBuy();
     }
 
-    //this changes the color depending on if you have enough money or not
+    /*
+     * This function changes the color depending on if you have enough money or not
+     */
     private void CanBuy()
     {
         if (!gameManager.Started) return;
@@ -116,22 +115,23 @@ public class DragAndDropIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     /**
      * Handle beginning of a drag action on this object
+     * @param eventData The PointerEventData for the drag which allows us to reference the pointer position for UpdateDrag
      */
     public void OnBeginDrag(PointerEventData eventData)
     {
         if (!gameManager.PlaceState) return;
         if (towerUI.CanAfford)
         {
-            // spawn the object
-            // keep it invisible and don't set position yet because we haven't actually figured out where it's going to go yet
-            // but, we need to instantiate it so we can figure out whether 
+            // Spawn the object
+            // Keep it invisible and don't set position yet because we haven't actually figured out where it's going to go yet,
+            // but we need to instantiate it so we can figure out whether 
             spawnedObject = Instantiate(objectToSpawn);
 
-            // set the spawned object to be on the unplaced object layer, keeping its old layer for future use
+            // Set the spawned object to be on the unplaced object layer, keeping its old layer for future use
             spawnedObjectLayer = spawnedObject.layer;
             Layers.MoveAllToLayer(spawnedObject, LayerMask.NameToLayer(Layers.IGNORE_RAYCAST_LAYER_NAME));
 
-            // get the IDragAndDrop interface for the spawned object
+            // Get the IDragAndDrop interface for the spawned object
             spawnedObjectDragAndDrop = spawnedObject.GetComponentInChildren<IDragAndDropObject>();
             if (spawnedObjectDragAndDrop == null)
             {
@@ -139,23 +139,24 @@ public class DragAndDropIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, I
                 return;
             }
             
-            // show any visualizations that may exist for the object
+            // Show any visualizations that may exist for the object
             PlacementVisualizationManager.Instance.DisplayVisualization(spawnedObjectDragAndDrop.GetType(), true);
 
-            // calculate the offsets needed to get the points at the corners of the spawned object's renderer's bounding box
+            // Calculate the offsets needed to get the points at the corners of the spawned object's renderer's bounding box
+            // NOTE: These are * 0.25f to allow for Anglers and Rangers to be placed closer to the shore of the river 
             if (spawnedObjectDragAndDrop is MonoBehaviour mb && mb.GetComponent<MeshRenderer>().bounds is Bounds bounds)
             {
-                spawnedObjectOffsetTopLeft = new Vector3(-1 * bounds.extents.x, 0, bounds.extents.z);
-                spawnedObjectOffsetTopRight = new Vector3(bounds.extents.x, 0, bounds.extents.z);
-                spawnedObjectOffsetBottomLeft = new Vector3(-1 * bounds.extents.x, 0, -1 * bounds.extents.z);
-                spawnedObjectOffsetBottomRight = new Vector3(bounds.extents.x, 0, -1 * bounds.extents.z);
+                spawnedObjectOffsetTopLeft = new Vector3(-1 * bounds.extents.x * 0.25f, 0, bounds.extents.z * 0.25f);
+                spawnedObjectOffsetTopRight = new Vector3(bounds.extents.x * 0.25f, 0, bounds.extents.z * 0.25f);
+                spawnedObjectOffsetBottomLeft = new Vector3(-1 * bounds.extents.x * 0.25f, 0, -1 * bounds.extents.z * 0.25f);
+                spawnedObjectOffsetBottomRight = new Vector3(bounds.extents.x * 0.25f, 0, -1 * bounds.extents.z * 0.25f);
             }
             else
             {
                 Debug.LogError("Attempting to spawn object from drag and drop that has no MeshRenderer!");
             }
             
-            // start moving the icon
+            // Start moving the icon
             UpdateDrag(eventData);
         }
         else
@@ -166,6 +167,7 @@ public class DragAndDropIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     /**
      * Handle middle of drag action on this object
+     * @param eventData The PointerEventData for the drag which allows us to reference the pointer position for UpdateDrag
      */
     public void OnDrag(PointerEventData eventData)
     {
@@ -175,6 +177,7 @@ public class DragAndDropIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     /**
      * Handle end of drag action on this object
+     * @param eventData The PointerEventData for the drag which allows us to reference the pointer position for UpdateDrag
      */
     public void OnEndDrag(PointerEventData eventData)
     {
@@ -195,48 +198,48 @@ public class DragAndDropIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         
         Vector3 cameraPosition = mainCamera.transform.position;
         
-        // create a ray from the camera towards the terrain
+        // Create a ray from the camera towards the terrain
         Ray primaryRay = mainCamera.ScreenPointToRay(data.position);
 
-        // create a list to hold all of the results from raycasts
+        // Create a list to hold all of the results from raycasts
         List<RaycastHit> secondaryHitInfo = new List<RaycastHit>();
 
-        // create variable to store initial raycast result data
+        // Create variable to store initial raycast result data
         RaycastHit primaryHitInfo;
 
-        // attempt a raycast on the terrain and floor (water tilemap) layers
+        // Attempt a raycast on the terrain and floor (water tilemap) layers
         bool hit = Physics.Raycast(primaryRay, out primaryHitInfo, Mathf.Infinity, raycastLayerMask);
         if (hit)
         {
-            // we hit something!
+            // We hit something!
 
-            // if we're in the editor, draw the ray
+            // If we're in the editor, draw the ray
             #if UNITY_EDITOR
             Debug.DrawRay(primaryRay.origin, primaryRay.direction * primaryHitInfo.distance, Color.green);
             #endif
 
-            // update the location of the spawned object to be at the point of the raycast hit
+            // Update the location of the spawned object to be at the point of the raycast hit
             spawnedObject.transform.position = primaryHitInfo.point;
 
-            // do some secondary raycasts to get more information
-            // this gets a bit complicated...
+            // Do some secondary raycasts to get more information
+            // This gets a bit complicated...
 
-            // figure out the origin for a ray that originates on the camera's y plane
+            // Figure out the origin for a ray that originates on the camera's y plane
             // and goes through the center of the drag and drop object at a 90 degree angle
             float raycastOriginDepth = cameraPosition.y - primaryHitInfo.point.y;
             
             Vector3 raycastOrigin = mainCamera.ScreenToWorldPoint(new Vector3(data.position.x, data.position.y, raycastOriginDepth));
 
-            // want to raycast from the camera y, so reset the y value
+            // Want to raycast from the camera y, so reset the y value
             raycastOrigin.y = cameraPosition.y;
 
-            // make modifications to this origin so that the new origins are at each corner of the drag and drop object's bounding box
+            // Make modifications to this origin so that the new origins are at each corner of the drag and drop object's bounding box
             Vector3 raycastOriginTopLeft = raycastOrigin + spawnedObjectOffsetTopLeft;
             Vector3 raycastOriginTopRight = raycastOrigin + spawnedObjectOffsetTopRight;
             Vector3 raycastOriginBottomLeft = raycastOrigin + spawnedObjectOffsetBottomLeft;
             Vector3 raycastOriginBottomRight = raycastOrigin + spawnedObjectOffsetBottomRight;
 
-            // do raycasts from each of these origins forward into the scene, storing their results
+            // Do raycasts from each of these origins forward into the scene, storing their results
             RaycastHit topLeftHitInfo;
             RaycastHit topRightHitInfo;
             RaycastHit bottomLeftHitInfo;
@@ -246,23 +249,24 @@ public class DragAndDropIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, I
             Physics.Raycast(raycastOriginBottomLeft, Vector3.down, out bottomLeftHitInfo, Mathf.Infinity, raycastLayerMask);
             Physics.Raycast(raycastOriginBottomRight, Vector3.down, out bottomRightHitInfo, Mathf.Infinity, raycastLayerMask);
 
-            // draw these raycasts for debugging purposes
+            // Draw these raycasts for debugging purposes
+            // NOTE: These will be visible immediately around the dragged tower in the Unity Scene whenever the pointer MOVES
             Debug.DrawRay(raycastOriginTopLeft, Vector3.down * (cameraPosition.y - topLeftHitInfo.point.y), Color.blue);
             Debug.DrawRay(raycastOriginTopRight, Vector3.down * (cameraPosition.y - topRightHitInfo.point.y), Color.blue);
             Debug.DrawRay(raycastOriginBottomLeft, Vector3.down * (cameraPosition.y - bottomLeftHitInfo.point.y), Color.blue);
             Debug.DrawRay(raycastOriginBottomRight, Vector3.down * (cameraPosition.y - bottomRightHitInfo.point.y), Color.blue);
 
-            // put the secondary raycasts in a list to send along to the drag and drop object
+            // Put the secondary raycasts in a list to send along to the drag and drop object
             secondaryHitInfo.Add(topLeftHitInfo);
             secondaryHitInfo.Add(topRightHitInfo);
             secondaryHitInfo.Add(bottomLeftHitInfo);
             secondaryHitInfo.Add(bottomRightHitInfo);
         }
 
-        // check if the current location of the spawn is valid
+        // Check if the current location of the spawn is valid
         bool locationValid = spawnedObjectDragAndDrop.PlacementValid(primaryHitInfo, secondaryHitInfo);
 
-        // if this is the final drag update, call a method to handle it
+        // If this is the final drag update, call a method to handle it
         if (finalUpdate)
         {
             FinalizeDrag(locationValid, primaryHitInfo, secondaryHitInfo);
@@ -278,22 +282,22 @@ public class DragAndDropIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, I
      */
     private void FinalizeDrag(bool locationValid, RaycastHit primaryHitInfo, List<RaycastHit> secondaryHitInfo)
     {
-        // hide any visualizations that may have existed for the object
+        // Hide any visualizations that may have existed for the object
         PlacementVisualizationManager.Instance.DisplayVisualization(spawnedObjectDragAndDrop.GetType(), false);
 
-        // if we are in a valid place, spawn the object
+        // If we are in a valid place, spawn the object
         if (locationValid)
         {
-            // place the spawned object
+            // Place the spawned object
             spawnedObjectDragAndDrop.Place(primaryHitInfo, secondaryHitInfo);
 
-            // make it visible and turn it on
+            // Make it visible and turn it on
             spawnedObject.SetActive(true);
 
-            // set the layer back to whatever it was before
+            // Set the layer back to whatever it was before
             Layers.MoveAllToLayer(spawnedObject, spawnedObjectLayer);
 
-            //if the spawnedObject is the salmon ladder, mark this as the first ladder for UpgradeManager
+            // If the spawnedObject is the salmon ladder, mark this as the first ladder for UpgradeManager
             if (towerUI.TowerType == TowerType.Ladder)
             {
                 ManagerIndex.MI.UpgradeManager.OriginalLadder = spawnedObject;
@@ -304,7 +308,7 @@ public class DragAndDropIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, I
                 ManagerIndex.MI.UpgradeManager.isAFisherman = true;
             }
 
-            //subtract the cost of the tower from your bank
+            // Subtract the cost of the tower from your bank
             towerUI.Purchase();
 
             if (isOneTimePurcahse)
@@ -314,13 +318,13 @@ public class DragAndDropIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
             spawnedObject = null;
         }
-        // if not in a valid place, destroy the spawned object
+        // If not in a valid place, destroy the spawned object
         else
         {
-            // destroy object
+            // Destroy object
             Destroy(spawnedObject);
 
-            // clear old references, reset values to default
+            // Clear old references, reset values to default
             spawnedObject = null;
             spawnedObjectDragAndDrop = null;
             spawnedObjectOffsetTopLeft = Vector3.zero;
