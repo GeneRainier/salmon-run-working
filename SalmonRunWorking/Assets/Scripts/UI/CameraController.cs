dcb87913e858e20fef979744b45b680534bbf0ea
GeneRainier <GeneRainier@gmail.com>
2021-07-13 17:36:55 -0400

    Reworked Camera Controller
    Rewrote the Camera Controller to work independent of TimeScale and to be able to rotate side to side when zoomed in. The system is completely keyboard driven for the sake of our intended platform (laptops / chromebooks)

--- SalmonRunWorking/Assets/Scripts/UI/CameraController.cs
+++ SalmonRunWorking/Assets/Scripts/UI/CameraController.cs
@@ -20,22 +20,21 @@ public class CameraController : MonoBehaviour {
 
     [Header("Camera Bounds")]
     [SerializeField] private MinMax bounds = null;     //< Farthest / closest the camera can zoom out. Min and Max values for x and y
-    public float height;                        //< The difference between the max and min y values of bounds for calculating rotational interpolation
+    public float height;                               //< The difference between the max and min y values of bounds for calculating rotational interpolation
 
     [Header("Speeds")]
     [SerializeField] private float zoomSpeed = 2.0f;   //< The speed at which you can zoom in or out
 
-    [SerializeField] private float lerpSpeed = 0.0f;   //< The speed at which the camera pans
-
-    private Vector3 target;     //< The position the camera is aimed at
+    private Vector3 target;           //< The position the camera is aimed at
     private Vector3 targetRotation;   //< The rotation the camera is currently at
-    private bool moving;        //< Is the camera moving?
-    private float timeFactor;   //< What time scale is the game in?
+    private bool moving;              //< Is the camera moving?
+    private float timeFactor;         //< What time scale is the game in?
 
     [SerializeField] private Vector3 initialPosition;       //< The location the camera is at when the level begins
     [SerializeField] private Vector3 initialRotation;       //< The rotation the camera is at when the level begins
     [SerializeField] private float initialXRotation;       //< The x rotation when the camera is fully zoomed out
     [SerializeField] private float zoomedXRotation;        //< The x rotation the camera has when it is fully zoomed in
+    private float interpolation = 1.0f;                     //< The percentage value we are at in the xAxis rotation Lerp
 
     /*
      * Awake is called after the initialization of gameobjects prior to the start of the game. This is used as an Initialization Function
@@ -47,16 +46,6 @@ public class CameraController : MonoBehaviour {
         initialXRotation = this.gameObject.transform.rotation.eulerAngles.x;
         zoomedXRotation = 30.0f;
         height = bounds.Max.y - bounds.Min.y;
-}
-
-    /**
-     * Update is called once every frame update
-     */
-    private void Update ()
-    {
-        // We UpdateTarget here to make the movement of the camera consistent across all TimeScales
-        // Movement should generally be handled in FixedUpdate for consistency across all scripts
-        UpdateTarget();
     }
 
     /*
@@ -66,33 +55,38 @@ public class CameraController : MonoBehaviour {
     {
         // We physically move the camera after the UI has been adjusted so that the tooltips do not flash when the camera moves
         UpdatePosition();
+
+        // Pressing the F key will reset the camera to it's original y position and Vector3 rotation
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ResetCameraRotation();
+        }
     }
 
-    /**
-     * Update camera position and zoom
+    /*
+     * Updates the position of the camera
      */
-    private void UpdateTarget()
+    private void UpdatePosition()
     {
-        if (!moving)
-        {
-            moving = true;
-            target = transform.position;
-        }
-
-        // Get camera's current pos
-        target = transform.position;
-
         // Figure out new zoom
-        // TODO: SCROLL MORE WHEN YOU ARE CLOSE TO THE EDGE
         float scroll = 12f * Input.GetAxis("Mouse ScrollWheel");
 
         // Look for keyboard inputs as well
         if (Input.GetButton("Zoom") || Input.GetButton("Zoom"))
         {
             scroll = 0.1f * 100.0f * Input.GetAxisRaw("Zoom");
+            transform.Translate(Vector3.down * scroll * zoomSpeed, Space.World);
         }
 
-        target.y += scroll * zoomSpeed * Time.unscaledDeltaTime;
+        // Search for rotation input
+        // We only want to turn the camera if we are zoomed in
+        if (Input.GetButton("Rotate"))
+        {
+            transform.RotateAround(transform.position, Vector3.up, 1.0f * Input.GetAxisRaw("Rotate"));
+        }
+
+        // Store the current rotation for when we apply the XRotation Lerp
+        Vector3 currentRotation = transform.rotation.eulerAngles;
 
         // Figure out how much distance the pan should cover
         // Multiplied by unscaledDeltaTime so pausing and fast forwarding does not affect camera movement
@@ -102,46 +96,40 @@ public class CameraController : MonoBehaviour {
         if (Input.GetButton("Vertical") || (panWithMouse &&
             (Input.mousePosition.y >= Screen.height - panBorderThickness || Input.mousePosition.y <= panBorderThickness)))
         {
-            target.z += panDistance * Input.GetAxisRaw("Vertical");
+            transform.Translate(Vector3.forward * panDistance * Input.GetAxisRaw("Vertical"), Space.World);
         }
         if (Input.GetButton("Horizontal") || (panWithMouse &&
             (Input.mousePosition.x <= panBorderThickness || Input.mousePosition.x >= Screen.width - panBorderThickness)))
         {
-            target.x += panDistance * Input.GetAxisRaw("Horizontal");
-        }
-
-        // Clamp Pan (XZ) / Zoom (Y) within boundaries
-        target = bounds.Clamp(target);
-    }
-
-    /*
-     * Updates the position of the camera
-     */
-    private void UpdatePosition()
-    {
-        if (moving)
-        {
-            if (transform.ComparePosition(target, 0.01f))
-            {
-                moving = false;
-                return;
-            }
-            transform.SmoothMoveTowards(target, lerpSpeed);
+            transform.Translate(Vector3.right * panDistance * Input.GetAxisRaw("Horizontal"), Space.World);
         }
 
         // Calculate the interpolation factor for the camera rotation
-        // This is handle in UpdatePosition rather than UpdateTarget to keep the Slerp effect smooth
-        float interpolation = (transform.position.y - bounds.Min.y) / (height);
+        // This is handled in UpdatePosition rather than UpdateTarget to keep the Lerp effect smooth
+        interpolation = (transform.position.y - bounds.Min.y) / (height);
         float xRotation = Mathf.Lerp(zoomedXRotation, initialXRotation, interpolation);
-        transform.rotation = Quaternion.Euler(xRotation, transform.rotation.y, transform.rotation.z);
+        transform.rotation = Quaternion.Euler(xRotation, currentRotation.y, currentRotation.z);
+
+        // Clamp Pan (XZ) / Zoom (Y) within boundaries
+        transform.position = bounds.Clamp(transform.position);
     }
 
     /*
      * Set the camera position and rotation back to the start of the level when the end of round button is clicked
      */
-    public void ResetCameraPosition()
+    public void ResetCamera()
     {
         this.gameObject.transform.position = initialPosition;
+        this.gameObject.transform.rotation = Quaternion.Euler(initialRotation);
+    }
+
+    /*
+     * Set the camera position and rotation back to their initial values during a round.
+     * This will set the position back to its maximum y and its rotation back to its initial values
+     */
+    public void ResetCameraRotation()
+    {
+        this.gameObject.transform.position = new Vector3(transform.position.x, initialPosition.y, transform.position.z);
         this.gameObject.transform.rotation = Quaternion.Euler(initialRotation);
     }
 }
