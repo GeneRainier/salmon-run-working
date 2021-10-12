8a00c68c2afeb68740155cb1fa7be26cfae9f248
DaveKarlavage <dkarlava@msu.edu>
2021-10-12 11:23:36 -0400

    Angler and Ranger Hats now have different colored hats
    So I had to change the Shader that the hats are created with in order to get this to work. Also the Salmon Run logo on the ranger hats change as well. I don't think I can stop that from happening without separating the hat and the logo into two different objects.

---
+++ SalmonRunWorking/Assets/Scripts/Other/RandomHatColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomHatColor : MonoBehaviour
+{
+
+    public List<Color> ColorList = new List<Color>() {
+        Color.red,
+        Color.black,
+        Color.cyan,
+        Color.gray,
+        Color.green,
+        Color.magenta,
+        Color.white,
+        Color.yellow,
+        Color.blue,
+        new Color(0.3f, 0.4f, 0.6f, 0.3f)};
+
+    void Start()
+    {
+        int x = Random.Range(0, 10);
+        gameObject.GetComponent<Renderer>().material.SetColor("_Color", ColorList[x]);
+
+    }
+
+    void ApplyMaterial(Color color, int targetMaterialIndex)
+    {
+    }
+}
