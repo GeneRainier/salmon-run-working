ce099bd2f2302c46f5fc36f167c99b6729ff4680
David Karlavage <dkarlava@cse.msu.edu>
2022-03-30 17:12:01 -0400

    Revert "Anim and bug fix"
    This reverts commit c4909120f6bd996ae528156ad2e2a25d53ba9242.

--- SalmonRunWorking/Assets/Scripts/Towers/AnglerTower.cs
+++ SalmonRunWorking/Assets/Scripts/Towers/AnglerTower.cs
@@ -84,10 +84,6 @@ public class AnglerTower : TowerBase
         {
             SetLinePos();
         }
-        else
-        {
-            fishing.SetBool(Fishing, false);
-        }
     }
 
     /**
